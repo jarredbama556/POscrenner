faf308dc021db2f63b8250070452116f37d5fe51
jarredbama556 <jarredalldredge@Jarreds-MacBook-Pro.local>
2015-01-12 14:43:39 -0600

    First commit, loads exif data, however missing the needed components.

--- POscrenner/ViewController.m
+++ POscrenner/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <AssetsLibrary/ALAsset.h>
+#import <AssetsLibrary/ALAssetRepresentation.h>
+#import <ImageIO/CGImageSource.h>
+#import <ImageIO/CGImageProperties.h>
 
 @interface ViewController ()
 
@@ -19,9 +23,62 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) loadImage {
 }
 
+- (IBAction)selectPhoto:(UIButton *)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *selectedImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image = selectedImage;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    [library assetForURL:[info objectForKey:UIImagePickerControllerReferenceURL]
+             resultBlock:^(ALAsset *asset) {
+                 
+                 ALAssetRepresentation *image_representation = [asset defaultRepresentation];
+                 
+                 // create a buffer to hold image data
+                 uint8_t *buffer = (Byte*)malloc(image_representation.size);
+                 NSUInteger length = [image_representation getBytes:buffer fromOffset: 0.0  length:image_representation.size error:nil];
+                 
+                 if (length != 0)  {
+                     
+                     // buffer -> NSData object; free buffer afterwards
+                     NSData *adata = [[NSData alloc] initWithBytesNoCopy:buffer length:image_representation.size freeWhenDone:YES];
+                     
+                     // identify image type (jpeg, png, RAW file, ...) using UTI hint
+                     NSDictionary* sourceOptionsDict = [NSDictionary dictionaryWithObjectsAndKeys:(id)[image_representation UTI] ,kCGImageSourceTypeIdentifierHint,nil];
+                     
+                     // create CGImageSource with NSData
+                     CGImageSourceRef sourceRef = CGImageSourceCreateWithData((CFDataRef) adata,  (CFDictionaryRef) sourceOptionsDict);
+                     
+                     // get imagePropertiesDictionary
+                     CFDictionaryRef imagePropertiesDictionary;
+                     imagePropertiesDictionary = CGImageSourceCopyPropertiesAtIndex(sourceRef,0, NULL);
+                     
+                     // get exif data
+                     CFDictionaryRef exif = (CFDictionaryRef)CFDictionaryGetValue(imagePropertiesDictionary, kCGImagePropertyExifDictionary);
+                     NSDictionary *exif_dict = (__bridge NSDictionary*)exif;
+                }
+                 else {
+                     NSLog(@"image_representation buffer length == 0");
+                 }
+             }
+            failureBlock:^(NSError *error) {
+                NSLog(@"couldn't get asset: %@", error);
+            }
+     ];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
 @end
